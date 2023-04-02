7ca44cd0a8747e7c1a72eaf7102f54ef3e1ff0fd
julioallday <naruto481@gmail.com>
2023-04-02 17:08:53 -0300

    Cria componentes, guardas de rotas e rotas.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,39 @@
+import { AcessoGuard } from './shared/guards/acesso.guard';
+import { SemAcessoComponent } from './sem-acesso/sem-acesso.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    canActivate: [AcessoGuard],
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    canActivate: [AcessoGuard],
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'sem-acesso',
+    component: SemAcessoComponent,
+  },
+  {
+    path: '**',
+    component: LoginComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
+
+// /(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[@!#$%^&*()/\\])/
---
+++ src/app/shared/guards/acesso.guard.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AcessoGuard implements CanActivate {
+  canAccessHome: boolean = false;
+
+  constructor(private router: Router) {}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    if (this.canAccessHome) return true;
+    this.router.navigate(['sem-acesso']);
+    return false;
+  }
+}
